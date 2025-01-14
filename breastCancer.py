3ee9b01046bc2c5e7b8d56aef13a37121070fa3d
Devan Nanda <dnanda@cps.edu>
2025-01-14 08:24:54 -0600

    1.14.25

--- breastCancer.py
+++ breastCancer.py
@@ -2,9 +2,10 @@ import pandas as pd
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
 
 data = pd.read_csv("bc/breast_cancer/wdbc_data.csv")
-data["Diagnosis"].replace(["M","B"],[0,1],inplace=True)
+data["Diagnosis"].replace(["1","0"],[1,0],inplace=True)
 
 x = data[["Radius", "Texture", "Smoothness"]].values
 y = data["Diagnosis"].values
@@ -12,15 +13,19 @@ y = data["Diagnosis"].values
 print(x)
 print(y)
 
-x = StandardScaler().fit(x).transform(x)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(x_train)
+X_test_scaled = scaler.transform(x_test)
 
 model = linear_model.LogisticRegression()
 model.fit(x_train, y_train)
 
+y_pred = model.predict(X_test_scaled)
+accuracy = accuracy_score(y_test, y_pred)
 print("accuracy: ", model.score(x_test, y_test))
 
-
+print(f"Accuracy: {accuracy}")
 predictions = model.predict(x_test)
 for i in range(len(y_test)): print("prediction: ", predictions[i], " Answer: ", y_test[i])
